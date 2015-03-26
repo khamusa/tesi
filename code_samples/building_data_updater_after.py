01b99565738018034afe04556c9a9053872413a7
Samuel Brandão <gb.samuel@gmail.com>
2015-03-26 21:00:27 +0100

    Sistemando commento di blocco di codice che sorpassava il margine destro

--- code_samples/building_data_updater_after.py
+++ code_samples/building_data_updater_after.py
@@ -1,8 +1,8 @@
 self.batch_date = datetime.now()
 
 for b_data in buildings:
-  if not self._validate_building_data(b_data):      # Step 1 - Righe 5-15 dell'originale
-    continue
+  if not self._validate_building_data(b_data):      # Step 1 - Righe 5-15 
+    continue                                        # dell'originale
 
   building = self.find_building_to_update(b_data)   # Step 2 - Righe 20-23
   self._mark_building_as_updated(building)          # Step 3 - Righe 26-28
